c1f36fc9b3b5b5770eb5e5f55d0c847d608d36a4
afar-rafa <rcalvo044@gmail.com>
2024-08-28 20:27:16 -0400

    Asset (activos) and Portfolio models and migrations

--- abacusAPI/apps/abacusapp/models.py
+++ abacusAPI/apps/abacusapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Portfolio(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Asset(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- abacusAPI/apps/abacusapp/views.py
+++ abacusAPI/apps/abacusapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Portfolio, Asset
+from .serializers import PortfolioSerializer, AssetSerializer
 
-# Create your views here.
+class PortfolioViewSet(viewsets.ModelViewSet):
+    queryset = Portfolio.objects.all()
+    serializer_class = PortfolioSerializer
+
+class AssetViewSet(viewsets.ModelViewSet):
+    queryset = Asset.objects.all()
+    serializer_class = AssetSerializer
